ee6a94a22dd64cb56526991f4990f6997d0d6d44
MostafaWaseemMahmoud <mostafawaseem22@gmail.com>
2024-04-15 17:17:15 +0200

    ADD Command Pattern And Finish Desgin Pattern

--- Beheviral-Pattern/command.js
+++ Beheviral-Pattern/command.js
@@ -1,49 +1,3 @@
-// // Receiver
-// class TV {
-//   turnOn() {
-//     console.log("TV is on");
-//   }
-
-//   turnOff() {
-//     console.log("TV is off");
-//   }
-// }
-
-// // Command interface
-// class Command {
-//   execute() {}
-// }
-
-// // Concrete commands
-// class TurnOnCommand extends Command {
-//   execute() {
-//     console.log("Turning TV on...");
-//   }
-// }
-
-// class TurnOffCommand extends Command {
-//   execute() {
-//     console.log("Turning TV off...");
-//   }
-// }
-
-// // Invoker
-// class RemoteControl {
-//   pressButton(command) {
-//     command.execute();
-//   }
-// }
-
-// // Usage
-// const tv = new TV();
-// const remote = new RemoteControl();
-
-// const turnOnCommand = new TurnOnCommand();
-// const turnOffCommand = new TurnOffCommand();
-
-// remote.pressButton(turnOnCommand); // Output: Turning TV on...
-// remote.pressButton(turnOffCommand); // Output: Turning TV off...
-
 class Tv {
   turnOn() {
     console.log("Tv Turn On");
@@ -74,19 +28,10 @@ class RemoteControl {
   }
 }
 
-// const tv = new TV();
-// const remote = new RemoteControl();
-
-// const turnOnCommand = new TurnOnCommand();
-// const turnOffCommand = new TurnOffCommand();
-
-// remote.pressButton(turnOnCommand); // Output: Turning TV on...
-// remote.pressButton(turnOffCommand); // Output: Turning TV off...
-
 const tv = new Tv();
 const remote = new RemoteControl();
 
 const turnOnCommand = new TurnOnCommand();
-remote.pressButton(turnOnCommand); // Output: Tv Turn On
+remote.pressButton(turnOnCommand);
 const turnOffCommand = new TurnOffCommand();
 remote.pressButton(turnOffCommand);
